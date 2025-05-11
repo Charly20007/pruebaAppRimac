c98155478c968288b2f3f5360201bb3ed9777c8d
Carlos Marcos <carlos@kuali.ai>
2025-05-11 18:33:59 -0500

    feat: add models

---
+++ src/core/models/plans.model.ts
@@ -0,0 +1,17 @@
+import type { IPlan } from "../interfaces";
+
+export class PlanOptionModel {
+
+    public static build(plan: IPlan, isPriceDiscount: boolean = false): IPlan {
+        const newPrice = plan.price - (plan.price * 0.05)
+        return {
+            name: plan.name,
+            price: plan.price,
+            description: plan.description,
+            age: plan.age,
+            priceDiscount: isPriceDiscount ? newPrice : undefined,
+            inHome: !plan.name.includes("Clínica"),
+            recommendedPlan: plan.price.toString() === "99"
+        };
+    }
+}
