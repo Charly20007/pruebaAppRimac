b6b8bc35d0e7f78cfbaee466754935b9ca0cddd6
Carlos Marcos <carlos@kuali.ai>
2025-05-11 18:27:22 -0500

    feat: update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import './main.css'
+import "./styles/styles.css";
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
-  </StrictMode>,
-)
+);
